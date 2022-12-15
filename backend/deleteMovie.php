f8eab3fc9216a23959f256320ba51e3d970b6f6d
EmilDok <kyrbicanal2@gmail.com>
2022-12-15 19:55:54 +0300

    wef

--- backend/deleteMovie.php
+++ backend/deleteMovie.php
@@ -8,5 +8,12 @@
 
     $movies = mysqli_query($connect, "DELETE FROM `movie` WHERE movie_id = $movie_id;");
 
+    $movies = mysqli_query($connect, "select * from movie;");
+
+    $movies = mysqli_fetch_all($movies);
+
+    header("Content-Type: application/json");
+    echo json_encode($movies);
+
 
 ?>
