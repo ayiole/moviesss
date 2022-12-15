6657457ed3b8153d12d4d6859a0a2d15573fbcc4
EmilDok <kyrbicanal2@gmail.com>
2022-12-15 05:59:23 +0300

    files

--- backend/addMovie.php
+++ backend/addMovie.php
@@ -4,6 +4,15 @@
 
     $body = json_decode(file_get_contents('php://input'), true);
 
+    
+    $path1 = 'image_url_poster/' . time() . $_FILES['upfile']['name'];
+
+    move_uploaded_file($_FILES['upfile']['tmp_name'], $path);
+
+    $path2 = 'image_url_side/' . time() . $_FILES['upfile2']['name'];
+
+    move_uploaded_file($_FILES['upfile2']['tmp_name'], $path2);
+
     $genres = $body['genres'];
     $name = $body['name'];
     $year = $body['year'];
@@ -13,26 +22,19 @@
     $image_url_poster = $body['image_url_poster'];
     $image_url_side = $body['image_url_side'];
 
+    header("Content-Type: application/json");
+
     $movies = mysqli_query($connect, "INSERT INTO `movie`(`movie_id`, `name`, `year`, `producer`, `duration`, `description`, `image_url_poster`, `image_url_side`)
-     VALUES (NULL,'$name','$year','$producer','$duration','$description','$image_url_poster',' $image_url_side')");
+     VALUES (NULL,'$name','$year','$producer','$duration','$description','$path1','$path2')");
+    
 
-    $movies = mysqli_fetch_all($movies);
+    $lastMovieId = $connect->insert_id;
 
-    for ($i=0; $i < count($genres); $i++) { 
+    for ($i=0; $i < count($genres); $i++) {
 
-    $addMovieToGenre = mysqli_query($connect, "INSERT INTO `genre_movie`(`id`, `movie_id`, `genre_id`)
-     VALUES (NULL,'$movie_id','$genre_id')");
-
-    $addMovieToGenre = mysqli_fetch_all($addMovieToGenre);
-
-    header("Content-Type: application/json");
-    echo '{"done":"pituh"}';
+        $addMovieToGenre = mysqli_query($connect, "INSERT INTO `genre_movie`(`id`, `movie_id`, `genre_id`)
+        VALUES (NULL,'$lastMovieId','$genres[$i]')");
     }
-
-
-
-    header("Content-Type: application/json");
-    echo '{"done":"pituh"}';
 
 
 ?>
