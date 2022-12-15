7b848c851d428c1deb8eec9242ddb04a4fb57e91
ayiole <wowertp@gmail.com>
2022-12-15 19:16:51 +0300

    Update addMovie php

--- backend/addMovie.php
+++ backend/addMovie.php
@@ -1,40 +1,34 @@
 <?php
 
-    require_once("connect.php");
+require_once("connect.php");
 
-    $body = json_decode(file_get_contents('php://input'), true);
 
-    
-    $path1 = 'image_url_poster/' . time() . $body['upfile']['name'];
+$path1 = 'image_url_poster/' . time() . $_FILES['upfile']['name'];
 
-    move_uploaded_file($body['upfile']['tmp_name'], $path);
+move_uploaded_file($_FILES['upfile']['tmp_name'], $path1);
 
-    $path2 = 'image_url_side/' . time() . $body['upfile2']['name'];
+$path2 = 'image_url_side/' . time() . $_FILES['upfile2']['name'];
 
-    move_uploaded_file($body['upfile2']['tmp_name'], $path2);
+move_uploaded_file($_FILES['upfile2']['tmp_name'], $path2);
 
-    $genres = $body['genres'];
-    $name = $body['name'];
-    $year = $body['year'];
-    $producer = $body['producer'];
-    $duration = $body['duration'];
-    $description = $body['description'];
-    $image_url_poster = $body['image_url_poster'];
-    $image_url_side = $body['image_url_side'];
+$genres = explode(",", $_POST['genres']);
+$name = $_POST['name'];
+$year = $_POST['year'];
+$producer = $_POST['producer'];
+$duration = $_POST['duration'];
+$description = $_POST['description'];
 
-    header("Content-Type: application/json");
+header("Content-Type: application/json");
+echo json_encode($_POST);
 
-    $movies = mysqli_query($connect, "INSERT INTO `movie`(`movie_id`, `name`, `year`, `producer`, `duration`, `description`, `image_url_poster`, `image_url_side`)
+$movies = mysqli_query($connect, "INSERT INTO `movie`(`movie_id`, `name`, `year`, `producer`, `duration`, `description`, `image_url_poster`, `image_url_side`)
      VALUES (NULL,'$name','$year','$producer','$duration','$description','$path1','$path2')");
-    
 
-    $lastMovieId = $connect->insert_id;
 
-    for ($i=0; $i < count($genres); $i++) {
+$lastMovieId = $connect->insert_id;
 
-        $addMovieToGenre = mysqli_query($connect, "INSERT INTO `genre_movie`(`id`, `movie_id`, `genre_id`)
+for ($i = 0; $i < count($genres); $i++) {
+
+  $addMovieToGenre = mysqli_query($connect, "INSERT INTO `genre_movie`(`id`, `movie_id`, `genre_id`)
         VALUES (NULL,'$lastMovieId','$genres[$i]')");
-    }
-
-
-?>
+}
